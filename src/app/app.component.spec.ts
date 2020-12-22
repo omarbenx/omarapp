4c8a7d855c1d3383acf32aa6805717bfb4aacbd5
omarbenx <60161831+omarbenx@users.noreply.github.com>
2020-12-22 02:53:55 +0100

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mohamedapp'`, () => {
+  it(`should have as title 'omarapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mohamedapp');
+    expect(app.title).toEqual('omarapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mohamedapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('omarapp app is running!');
   });
 });
